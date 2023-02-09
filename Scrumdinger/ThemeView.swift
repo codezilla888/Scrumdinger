11c2ad79b5f38bd71e6be419dcf905f5e8082c67
Bagrat <bagrat.arutyunov.888@gmail.com>
2023-02-09 21:05:36 +0300

    Passing data with bindings

---
+++ Scrumdinger/ThemeView.swift
@@ -0,0 +1,28 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Bagrat Arutyunov on 09.02.2023.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .fill(theme.mainColor)
+            Label(theme.name, systemImage: "paintpalette")
+                .padding(4)
+        }
+        .foregroundColor(theme.accentColor)
+        .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
